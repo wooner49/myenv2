f7ca14b0b366c3c0a3c287191b4c15e25eb42cc6
wooner49 <wooner49@gmail.com>
2019-09-06 23:27:13 +0900

    add tmux setting

--- mybin/env_init.sh
+++ mybin/env_init.sh
@@ -5,6 +5,13 @@ RC_PATH=$HOME/.zshrc
 PP=`grep 'source $HOME/.myenv' $RC_PATH`
 [ ! -z "$PP" ] && echo ".myenv is already registered." || echo 'source $HOME/.myenv' >> $RC_PATH
 
-
 source $RC_PATH
+
+cd $HOME
+cfg submodule init
+cfg submodule update
+
+ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
+cp ~/.tmux/.tmux.conf.local .
+
 echo "--------------- Finish Configuration ---------------"
