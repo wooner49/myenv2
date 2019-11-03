762771270e19835ac959d77b2ca7493f49d4eec6
wooner49 <wooner49@gmail.com>
2019-11-03 17:54:23 +0900

    auto symbolic link

---
+++ bin/symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="`pwd`/.."
+ln -s -f $DIR/.vimrc ~/.vimrc 
+ln -s -f $DIR/.gitconfig ~/.gitconfig
+ln -s -f $DIR/.gitignore ~/.gitignore 
+ln -s -f $DIR/.tmux/.tmux.conf ~/.tmux.conf
+ln -s -f $DIR/.tmux/.tmux.conf.local ~/.tmux.conf.local
