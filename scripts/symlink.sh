4ed66869e3d205631c1c626c5a275a053eb29bc0
wooner49 <wooner49@gmail.com>
2019-12-07 00:27:30 +0900

    add some macro functions

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -7,3 +7,14 @@ ln -s -f $DIR/.gitignore ~/.gitignore
 ln -s -f $DIR/.tmux/.tmux.conf ~/.tmux.conf
 ln -s -f $DIR/.tmux/.tmux.conf.local ~/.tmux.conf.local
 ln -s -f $DIR/.myenv ~/.myenv
+
+STR='if [ -f ~/.myenv ]; then source ~/.myenv; fi'
+case "$OSTYPE" in
+    linux*)
+        echo $STR >> ~/.bashrc
+        ;;
+    darwin*)
+        echo $STR >> ~/.zshrc
+        ;;
+    *) ;;
+esac
